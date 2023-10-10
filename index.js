f3b003014aea127009767a1549cbfbf6325d38ec
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2023-10-10 17:07:29 +0100

    unicorn plugin rules pt2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 console.log('abc', 'def');
 const callback = (x) => x + 1;
 const array = [1, 2, 3];
@@ -5,3 +7,17 @@ array.map((element) => callback(element));
 for (const element of array) {
     callback(element);
 }
+for (const [index, element] of array.entries()) {
+    console.log(index, element);
+}
+
+const foo = '\u001B';
+const bar = '\u001B';
+const baz = '\u001B';
+const isArray = Array.isArray(array);
+
+const length = 10;
+const array2 = Array.from({ length });
+
+const buffer = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]);
+const un = undefined;
